553f04c9ac2cc113c1b2feeace3b0358d69ea3b1
Harish1685 <kumarharish1680@gmail.com>
2026-07-01 12:06:27 +0530

    added vpc.tf and provider.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,26 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = local.name
+  cidr = local.vpc_cidr
+  azs = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false
+  
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+   # Ensure public subnets auto-assign public IPs
+  map_public_ip_on_launch = true
+
+  
+}
